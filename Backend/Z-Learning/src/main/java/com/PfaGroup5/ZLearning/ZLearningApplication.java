353448c64515469b231179ab31953e743bba2fd4
Ahmed Warid <mwar662002@gmail.com>
2024-04-08 00:43:54 +0000

    spring boot project

---
+++ Backend/Z-Learning/src/main/java/com/PfaGroup5/ZLearning/ZLearningApplication.java
@@ -0,0 +1,13 @@
+package com.PfaGroup5.ZLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZLearningApplication.class, args);
+	}
+
+}
---
+++ Backend/Z-Learning/src/test/java/com/PfaGroup5/ZLearning/ZLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PfaGroup5.ZLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
