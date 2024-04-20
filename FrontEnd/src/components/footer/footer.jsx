f1468d5f721de80eeb770fc0dc0c1e2a8f9e35e2
Yahya LAZREK <uu.soft.inc@gmail.com>
2024-04-20 17:29:33 +0100

    initialize frontend web

---
+++ FrontEnd/src/components/footer/footer.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+
+function Header() {
+
+  return (
+    <>Footer</>
+  )
+}
+
+export default Header
---
+++ FrontEnd/src/components/header/header.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+
+function Header() {
+
+  return (
+    <>ZLearning</>
+  )
+}
+
+export default Header
