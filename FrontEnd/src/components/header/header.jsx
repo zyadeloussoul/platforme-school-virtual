a7d99a35ac0d2e8b4954e37bdae6a3b1d43585ac
Yahya LAZREK <uu.soft.inc@gmail.com>
2024-04-25 18:09:21 +0100

    gather information in newsletter

--- FrontEnd/src/components/header/header.jsx
+++ FrontEnd/src/components/header/header.jsx
@@ -5,26 +5,53 @@ import { useState } from 'react'
 import CustomLink from "../CustomLink/CustomLink";
 
 function Header() {
+  const [isPanelVisible, setPanelVisible] = useState(false);
+
+  const category = [
+    "Art",
+    "Architecture",
+    "Finance",
+    "Science",
+    "Psychologie"
+  ];
 
   return (
-    <div className={styles.container}>
-      <div className={styles.logo}>
-        <a href="#">ZLearning</a>
+    <div>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <a href="#">ZLearning</a>
+        </div>
+
+        <div className={styles.list}>
+          <ul>
+            <li><CustomLink href="#home">Accueil</CustomLink></li>
+            <li onMouseEnter={() => setPanelVisible(true)}
+                onMouseLeave={() => setPanelVisible(false)}>
+                <CustomLink href="#courses">Cours</CustomLink>
+            </li>
+            <li><CustomLink href="#about">À propos</CustomLink></li>
+            <li><CustomLink href="#newsletter">Newsletter</CustomLink></li>
+          </ul>
+        </div>
+
+        <div className={styles.auth}>
+          <button className="btn-v2">Se connecter</button>
+          <button>S’inscrire</button>
+        </div>
       </div>
 
-      <div className={styles.list}>
+      {/* Courses sub-navbar */}
+      {isPanelVisible && (
+      <div className={styles.panel}
+          onMouseEnter={() => setPanelVisible(true)}
+          onMouseLeave={() => setPanelVisible(false)}>
         <ul>
-          <li><CustomLink href="#home">Accueil</CustomLink></li>
-          <li><CustomLink href="#courses">Cours</CustomLink></li>
-          <li><CustomLink href="#about">À propos</CustomLink></li>
-          <li><CustomLink href="#contact">Contact</CustomLink></li>
+        {category.map((c, index)=>(
+          <li key={index}><a href={`#${c}`}>{c}</a></li>
+        ))}
         </ul>
       </div>
-
-      <div className={styles.auth}>
-        <button className="btn-v2">Se connecter</button>
-        <button>S’inscrire</button>
-      </div>
+      )}
     </div>
   )
 }
--- FrontEnd/src/components/newsletter/newsletter.jsx
+++ FrontEnd/src/components/newsletter/newsletter.jsx
@@ -1,18 +1,25 @@
 import styles from "./Newsletter.module.css";
+import { useState } from "react";
 
 function Newsletter() {
+  const [phone, setPhone] = useState("");
+
+  const handleSubmit = ()=>
+  {
+    alert("saving: "+phone);
+  }
 
   return (
-    <div className={styles.container}>
+    <div id="newsletter" className={styles.container}>
       <div className={styles.background}>
         <h2>Abonnez-vous à notre newsletter</h2>
         <p>Abonnez-vous à notre newsletter pour obtenir des informations sur nos cours.</p>
 
         <div className={styles.details}>
           <div className={styles.input}>
-            <input type="number" name="phone" placeholder="Entrez votre numéro de téléphone"/>
+            <input type="number" name="phone" placeholder="Entrez votre numéro de téléphone" onChange={(e)=>setPhone(e.target.value)}/>
           </div>
-          <button>Abonnez-vous</button>
+          <button onClick={handleSubmit}>Abonnez-vous</button>
         </div>
       </div>
     </div>
