0eab9216c4c0699a11d8a6c67a141d2f76eb0480
Yahya LAZREK <uu.soft.inc@gmail.com>
2024-04-26 15:55:04 +0100

    port change

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css'
 
 //components
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
 import Course from './components/Course/Course';
 import Newsletter from './components/Newsletter/Newsletter';
